ce274ce08c5d9def9a0a7d67371e2e758a69f283
Sin Wildy <sw.wildy@gmail.com>
2020-10-23 18:54:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,7 +11,9 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  division_total = num1 % num2
+  division_total = num1 / num2
+  puts division_total
+  return division_total
 end
 
 def multiplication(num1, num2)
