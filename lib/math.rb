e23b2f99f09d6b183957d230c1bc5077a49ee1e6
Sin Wildy <sw.wildy@gmail.com>
2020-10-23 19:06:27 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  addition_total = num1 + num2
+  puts addition_total
+  return addition_total
 end
 
 def subtraction(num1, num2)
-
+  subtraction_total = num1 - num2
+  puts subtraction_total
+  return subtraction_total
 end
 
 def division(num1, num2)
-
+  division_total = num1 / num2
+  puts division_total
+  return division_total
 end
 
 def multiplication(num1, num2)
-
+  multiplication_total = num1 * num2
+  puts multiplication_total
+  return multiplication_total
 end
 
 def modulo(num1, num2)
-
+  modulo_total = num1 % num2
+  puts modulo_total
+  return modulo_total
 end
 
 def square_root(num)
-
+  square_root_total = Math.sqrt(num)
+  puts square_root_total
+  return square_root_total
 end
